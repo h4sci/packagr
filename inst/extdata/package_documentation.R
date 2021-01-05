6a4d552de3a69e93e6e387e61e2ad57a4a346012
Philipp Kronenberg <kronenberg@kof.ethz.ch>
2021-01-05 14:16:06 +0100

    tidy up

--- inst/extdata/package_documentation.R
+++ inst/extdata/package_documentation.R
@@ -6,24 +6,21 @@
 # jekyll/update/2015/07/26/making-your-first-R-package.html
 
 # Install packages
-  install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
-  install.packages("git2r")
-  install.packages("usethis")
-# Create Framework (NAMESPACE, DESCRIPTION,R, myfirstpackage.Rproj)
+  install.packages(c("devtools", "roxygen2", "testthat", "knitr", "usethis", "git2r"))
+  
+# Create Framework (NAMESPACE, DESCRIPTION, R, myfirstpackage.Rproj)
   devtools::create("myfirstpackage")
-# creates automatically a new project in R
+  #creates automatically a new project in R
 
-  # Create a git folder
+# Create a git folder
   #use_git() # Get an error somehow?
 
-  # Change DESCRIPTION text
-  # Change license
+# Change DESCRIPTION text by hand
+  
+# Change license
   use_mit_license("Philipp Kronenberg")
 
-# Add dependencies to package # Defaults to imports
-  #install.packages(c("reshape2", "MASS", "stats", "ggplot2","openxlsx",
-  #                   "MCMCpack","Matrix","dplyr","zoo","tidyr"))
-
+# Add dependencies to package (Defaults to imports)
   usethis::use_package("MASS")
   usethis::use_package("stats")
   usethis::use_package("utils")
@@ -37,94 +34,93 @@
   usethis::use_package("tidyr")
   usethis::use_package("grDevices")
 
-
-  #> Adding dplyr to Imports
-  #> Refer to functions with dplyr::fun()
-  # usethis::use_package("data.table", "Suggests")
-  #> Use requireNamespace("data.table", quietly = TRUE) to test if package is
-  #>  installed, then use data.table::fun() to refer to functions.
+# Notes about dependencies:
+  # Refer to functions with dplyr::fun()
+  # Or import the package/functions for the whole file.
+  # Use requireNamespace("data.table", quietly = TRUE) to test if package is
+  # installed, then use data.table::fun() to refer to functions.
 
 # Add function description
   # press: Ctrl + Shift + Alt + R for basic roxygen sceleton including all parameters.
 
-# Update function description: Add .Rd file to man folder
+# Update function description: Add .Rd files for all functions to man folder
   #install.packages("ps")
   devtools::document()
   # Each time you add new documentation to your R function,
   # you need to run devtools::document() again to re-generate the .Rd files.
 
-# Create some sample data object and save it in data folder and inst/extdata to
-# provide data code
-  x <- c(1:10)
-  usethis::use_data(x)
+# Create some sample data object and save it 
+# in data folder and inst/extdata to provide data code, e.g.:
+  # x <- c(1:10)
+  # usethis::use_data(x)
   # How to call this data:
   #system.file("extdata", "x.R", package = "myfirstpackage")
 
-  # Create basic read me file for git
+# Create basic read me file for git
   use_readme_rmd()
 
-  # set up data-raw folder
+# set up data-raw folder
   usethis::use_data_raw()
 
+# Create tests
+  use_testthat() # Creates test folder
+  use_test("get_IC") # creates test file for single function
+  
+  
 # Create vignette rmarkdown file
   # installing/loading the package:
-  if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
+  if(!require(installr)) { install.packages("installr"); require(installr)} 
   # Installing pandoc
   install.pandoc()
   rmarkdown::find_pandoc()
-
   install.packages("jsonlite")
   install.packages("rmarkdown")
+  # Create new vignette (overrides old vignette file):
   usethis::use_vignette("introduction")
-  #install pandoc
-
 
 # Install your package
-  devtools::install()
-  # or
+  devtools::install() # or
   devtools::load_all() # replaces installation, and attachement of package
+  
   # remove.packages(vctrs)
 
-  # Install a package from github:
-  # devtools::install_github("yourusername/myfirstpackage")
+# Install a package from github:
+  # devtools::install_github("yourusername/packagename")
 
-  # Install from github:
-  # devtools::install_github("yourusername/myfirstpackage")
+# To use when revising the package ---------------------------------------
 
-
-# To use when revisiong the package ---------------------------------------
-
-
+# load necessary packages
   library(devtools)
   packageVersion("devtools")
   library(roxygen2)
   library(testthat)
   devtools::session_info()
-  library(tidyverse)
+  #library(tidyverse)
   library(fs)
 
+# update documentation if changes made
   devtools::document()
 
+# load package
   load_all()
 
-  devtools::run_examples() # if problems with examples ...
-
+# attach package
+  library("BayesianDFM")
+  
+# rebuild vignette
+  devtools::build_vignettes() # local I get an error because of pandoc
+  
+# make basic check whether the package works (including test() and run_examples())
+  check() 
+  # runs examples if problems occur in check()
+  devtools::run_examples()
+  # Check documentation
+  devtools::check_man()
   # Test your functions
-  use_testthat() # Creates test folder
-  use_test("get_IC") # creates test file for single function
   test()
 
-  # make basic check whether the package works
-  check() # includes the command test() that checks also your own created tests
-  # devtools::check_man() # if problems with documentation instead of always
-  # using check
-
-  library("BayesianDFM")
-
-  # Create package including Create/update Vignette
-  devtools::build()
-
-
+# Create package including Create/update Vignette
+  devtools::build() # local I get an error because of pandoc
 
   # Render your read me file
   build_readme()
